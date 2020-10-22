5d264c0f1b53a1998aaf01c8a00ac5b7afc82ad7
thewall <309984394@qq.com>
2020-10-22 18:30:57 +0800

    hash

--- src/router.js
+++ src/router.js
@@ -5,7 +5,8 @@ import Router from "vue-router";
 Vue.use(Router);
 
 export default new Router({
-    mode: 'history',
+    // mode: 'history',
+    mode:'hash',
     routes: [{
         path: "/",
         name: "home",
